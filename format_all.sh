5b5a0ba4051654da69808a7d3cc9d08c5041a6e2
LED-reamer <tim.hertler@gmx.de>
2025-08-08 20:23:54 +0200

    update formatting

--- format_all.sh
+++ format_all.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Überprüfen, ob .c oder .h Dateien im aktuellen Verzeichnis oder Unterverzeichnissen existieren, aber ohne den .ccls-cache Ordner
 FILES=$(find . -type d -name ".ccls-cache" -prune -o -type f \( -iname "*.c" -o -iname "*.h" \) -print)
 
-# Wenn keine Dateien gefunden werden
 if [ -z "$FILES" ]; then
   echo "Keine .c oder .h Dateien gefunden."
   exit 1
 fi
 
-# Durchlaufen der gefundenen Dateien und formatieren
 for FILE in $FILES; do
   if [ -f "$FILE" ]; then
-    ./format.sh "$FILE"  # Aufruf von format.sh mit der jeweiligen Datei
+    clang-format -i -style="{BasedOnStyle: Google, IndentWidth: 4, TabWidth: 4, UseTab: Always, ColumnLimit: 0}" "$FILE"
+    echo "formatted: $FILE"
   fi
 done
