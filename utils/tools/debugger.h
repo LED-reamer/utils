a8d04f73dad445dbdeff53769e66ad317802ea07
LED-reamer <tim.hertler@gmx.de>
2025-08-19 12:10:17 +0200

    update

--- utils/tools/debugger.h
+++ utils/tools/debugger.h
@@ -4,3 +4,7 @@
 void debugger_init();
 void debugger_update();
 void debugger_deinit();
+
+//TODO:
+//void debugger_timestamp_begin(const char* name);
+//void debugger_timestamp_end(const char* name);
