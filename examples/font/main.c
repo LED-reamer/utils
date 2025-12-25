7e315cf24658c9932cdecc7de920b087189885ac
LED-reamer <tim.hertler@gmx.de>
2025-12-25 19:04:13 +0100

    update

--- examples/font/main.c
+++ examples/font/main.c
@@ -13,17 +13,17 @@ int main(void) {
 
 	font_t font = font_create_from_file(allocator_get_default(), filename, 24.0f);
 
-	for (size_t i = 0; i < font.num_glyphs; i++) {
-		LOG("glyph (codepoint %i) is at reactangle(%u, %u, %u, %u), advance_width = %i, left_space = %i",
-			font.glyphs[i].codepoint, font.glyphs[i].src_x, font.glyphs[i].src_y, font.glyphs[i].src_width, font.glyphs[i].src_height, font.glyphs[i].advance_width, font.glyphs[i].left_space);
-	}
-	WARNING("NOTE src_width is bigger than advance_width since with is the size of the sdf bitmap (which contains a buffer zone)");
+	//for (size_t i = 0; i < font.num_glyphs; i++) {
+	//	LOG("glyph (codepoint %i) is at reactangle(%u, %u, %u, %u), advance_width = %i, left_space = %i",
+	//		font.glyphs[i].codepoint, font.glyphs[i].src_x, font.glyphs[i].src_y, font.glyphs[i].src_width, font.glyphs[i].src_height, font.glyphs[i].advance_width, font.glyphs[i].left_space);
+	//}
+	//WARNING("NOTE src_width is bigger than advance_width since with is the size of the sdf bitmap (which contains a buffer zone)");
 
-	LOG("font global info: font_size=%f, ascent=%i, descent=%i, line_height=%i, num_glyphs=%i", font.font_size, font.ascent, font.descent, font.line_height, font.num_glyphs);
+	//LOG("font global info: font_size=%f, ascent=%i, descent=%i, line_height=%i, num_glyphs=%i", font.font_size, font.ascent, font.descent, font.line_height, font.num_glyphs);
 
-	uint32_t w, h;
-	font_string_size(&font, "Hello World", &w, &h);
-	LOG("size for \"Hello World\" is w=%u, h=%u", w, h);
+	float w, h;
+	font_string_size(&font, "Hello World", 16, &w, &h);
+	LOG("size for \"Hello World\" is w=%f, h=%f", w, h);
 
 	// test save fontatlas as image
 	image_t img = image_create(allocator_get_default(), font.atlas_width, font.atlas_height, IMAGE_GREY, IMAGE_8BIT);
--- examples/renderer_gl_impl/main.c
+++ examples/renderer_gl_impl/main.c
@@ -10,14 +10,12 @@ int main(void) {
 	memory_tracker_init(false);
 	window_t win = window_create_opengl("renderer", 800, 600);
 	renderer_init(allocator_get_default(), &win, RENDERER_2D_SHAPES | RENDERER_TEXT | RENDERER_3D_SHAPES);
-	//renderer_init(allocator_get_default(), &win, RENDERER_3D_SHAPES);
 	font_t font = font_create_from_file(allocator_get_default(), "font.ttf", 128.0f);
 	renderer_set_font(&font);
 
 	camera_t camera = renderer_get_default_camera();
-
-	while (window_open(&win)) {
 	
+	while (window_open(&win)) {
 		window_update(&win);
 
 		renderer_draw_line(vec2(0, 0), mouse_get_position(&win), 5, color(0, 0, 0, 1));
@@ -27,14 +25,10 @@ int main(void) {
 		renderer_draw_rectangle_size(vec2(mouse_get_position(&win).x, mouse_get_position(&win).y), vec2(w, h), color(1, 0, 0, .2));
 		renderer_draw_text(string, vec2(0, 0), 64, color(0, 0, 0, 1));
 
-		renderer_draw_cylinder(vec3(0, 0, 0), vec3(1, 0, 0), .1, color(1, 0, 0, 1));
-		renderer_draw_cylinder(vec3(0, 0, 0), vec3(0, 1, 0), .1, color(0, 1, 0, 1));
-		renderer_draw_cylinder(vec3(0, 0, 0), vec3(0, 0, 1), .1, color(0, 0, 1, 1));
+		renderer_draw_cylinder(vec3(0, 0, 0), vec3(1, 0, 0), .05, color(1, 0, 0, 1));
+		renderer_draw_cylinder(vec3(0, 0, 0), vec3(0, 1, 0), .05, color(0, 1, 0, 1));
+		renderer_draw_cylinder(vec3(0, 0, 0), vec3(0, 0, 1), .05, color(0, 0, 1, 1));
 
-
-		//TODO: depth testing is still wrong
-		renderer_draw_cylinder(vec3(-1, 0, 10), vec3(1, 0, 10), .1, color(.5, .5, .5, 1));
-		renderer_draw_cylinder(vec3(0, 1, 10), vec3(0, -1, 10), .1, color(.2, .2, .2, 1));
 
 		renderer_render(vec2(window_get_size(&win).x, window_get_size(&win).y), color(1, 1, 1, 1), camera);
 
